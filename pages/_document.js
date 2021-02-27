f684066631e368027a345ad99402f0e427a73772
Sergio Lo Porto <51946947+sergioloporto@users.noreply.github.com>
2021-02-27 21:43:06 +0100

    add FB and GA analytics

--- pages/_document.js
+++ pages/_document.js
@@ -3,8 +3,8 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 class MyDocument extends Document {
     render() {
         return (
-            <Html lang="en">
-                <Head>
+            <Html lang="pl">
+                <Head prefix="og: http://ogp.me/ns# book: http://ogp.me/ns/book#">
                     <link rel="icon" type="image/png" href="/images/favicon.png"></link>
                 </Head>
                 <body>
