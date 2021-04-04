86a14b1793a9ff2e8168a939fa6e032d07db0906
Sergio Lo Porto <51946947+sergioloporto@users.noreply.github.com>
2021-04-04 14:40:58 +0200

    finished sklep

--- components/BookLanding/Pricing.js
+++ components/BookLanding/Pricing.js
@@ -24,20 +24,64 @@ class Pricing extends Component {
     return (
       <section className="pricing-area book-pricing ptb-100 sklep-prices">
         <div className="container">
-          <div className="section-title">
+          {/* <div className="section-title">
             <h2>Easy Pricing Plans</h2>
             <div className="bar"></div>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua.
             </p>
-          </div>
+          </div> */}
+
+
+
 
           <div className="row">
-            <div className="col-lg-4 col-md-6">
+
+          <div className="col-lg-6 col-md-6">
               <div className="pricing-table">
+              <img src={finalPath + '/images/sekrety-rozwoju-osobistego-papier-gr.jpg'} alt="screenshot" />
                 <div className="price-header">
-                  <h3 className="title">e-Book</h3>
+                  <h3 className="title">KSIĄŻKA <br /> "Sekrety Rozwoju Osobistego"</h3>
+                  <div className="price">
+                    {buingOptions.paperCopy.price.toFixed(2)}<span className="dollar">zł</span>
+                  </div>
+                </div>
+
+                <div className="price-footer">
+                  <a className="btn btn-primary" href="/checkout?product=paperCopy"
+                  onClick={this.showLoader.bind(this)}>
+                    Zamawiam
+                  </a>
+                </div>
+              </div>
+            </div>
+
+          <div className="col-lg-6 col-md-6">
+              <div className="pricing-table">
+              <img src={finalPath + '/images/sekrety-rozwoju-osobistego-ebook-gr.jpg'} alt="screenshot" />
+                <div className="price-header">
+                  <h3 className="title">EBOOK <br /> "Sekrety Rozwoju Osobistego"</h3>
+                  <div className="price">
+                  {buingOptions.ebook.price.toFixed(2)}<span className="dollar">zł</span>
+                  </div>
+                </div>
+
+                <div className="price-footer">
+                  <a className="btn btn-primary" href="/checkout?product=paperCopy"
+                  onClick={this.showLoader.bind(this)}>
+                    Zamawiam
+                  </a>
+                </div>
+              </div>
+            </div>
+
+
+            <div className="col-lg-6 col-md-6">
+              <div className="pricing-table">
+              <img src={finalPath + '/images/radykalna-prawda-w-zwiazku.jpg'} alt="screenshot" />
+                <div className="price-header">
+                <h3 className="title">EBOOK <br /> "Radykalna Prawda w Związku"</h3>
                   <div className="price">
                     {buingOptions.ebook.price.toFixed(2)}<span className="dollar">zł</span>
                   </div>
@@ -55,29 +99,34 @@ class Pricing extends Component {
               </div>
             </div>
 
-            <div className="col-lg-4 col-md-6">
+
+
+            <div className="col-lg-6 col-md-6 offset-lg-0 offset-md-3">
               <div className="pricing-table">
-              <img src={finalPath + '/images/ksiazka.png'} alt="screenshot" />
+                <img src={finalPath + '/images/ksiazka-bundle.jpg'} alt="screenshot" />
                 <div className="price-header">
-                  <h3 className="title">Printed</h3>
+                  <h3 className="title">PAKIET <br /> KSIĄŻKA PAPIEROWA + EBOOK + RADYKALNA PRAWDA W ZWIĄZKU</h3>
                   <div className="price">
-                    {buingOptions.paperCopy.price.toFixed(2)}<span className="dollar">zł</span>
+                    {buingOptions.bundle.price.toFixed(2)}<span className="dollar">zł</span>
                   </div>
                 </div>
 
                 <div className="price-footer">
-                  <a className="btn btn-primary" href="/checkout?product=paperCopy"
+                  <a className="btn btn-primary" href="/checkout?product=bundle"
                   onClick={this.showLoader.bind(this)}>
                     Zamawiam
                   </a>
                 </div>
               </div>
             </div>
+          
 
-            <div className="col-lg-4 col-md-6 offset-lg-0 offset-md-3">
+            <div className="col-lg-6 col-md-6 offset-lg-0 offset-md-3">
               <div className="pricing-table">
+                <img src={finalPath + '/images/droga-do-szczescia.jpg'} alt="screenshot" />
                 <div className="price-header">
-                  <h3 className="title">Bundle</h3>
+                  <h3 className="title">KURS ONLINE<br /> PROGRAM: DROGA DO SZCZĘŚCIA</h3>
+                  <span className="readmore"><a href="/droga-do-szczescia">➜ Czytaj więcej o programie</a></span>
                   <div className="price">
                     {buingOptions.bundle.price.toFixed(2)}<span className="dollar">zł</span>
                   </div>
@@ -93,17 +142,19 @@ class Pricing extends Component {
             </div>
 
 
-            <div className="col-lg-4 col-md-6 offset-lg-0 offset-md-3">
+            <div className="col-lg-6 col-md-6 offset-lg-0 offset-md-3">
               <div className="pricing-table">
+                <img src={finalPath + '/images/droga-do-szczescia-vip.jpg'} alt="screenshot" />
                 <div className="price-header">
-                  <h3 className="title">Radykalna prawda w związku</h3>
+                  <h3 className="title">KURS ONLINE<br /> PROGRAM: DROGA DO SZCZĘŚCIA vip</h3>
+                  <span className="readmore"><a href="/droga-do-szczescia">➜ Czytaj więcej o programie</a></span>
                   <div className="price">
-                    {buingOptions.test.price.toFixed(2)}<span className="dollar">zł</span>
+                    {buingOptions.bundle.price.toFixed(2)}<span className="dollar">zł</span>
                   </div>
                 </div>
 
                 <div className="price-footer">
-                  <a className="btn btn-primary" href="/checkout?product=test"
+                  <a className="btn btn-primary" href="/checkout?product=bundle"
                   onClick={this.showLoader.bind(this)}>
                     Zamawiam
                   </a>
@@ -111,23 +162,7 @@ class Pricing extends Component {
               </div>
             </div>
 
-            <div className="col-lg-4 col-md-6 offset-lg-0 offset-md-3">
-              <div className="pricing-table">
-                <div className="price-header">
-                  <h3 className="title">Czy grupa jest sektą <br /> test</h3>
-                  <div className="price">
-                    {buingOptions.czygrupajestsekta.price.toFixed(2)}<span className="dollar">zł</span>
-                  </div>
-                </div>
 
-                <div className="price-footer">
-                  <a className="btn btn-primary" href="/checkout?product=test"
-                  onClick={this.showLoader.bind(this)}>
-                    Zamawiam
-                  </a>
-                </div>
-              </div>
-            </div>
 
 
           </div>
