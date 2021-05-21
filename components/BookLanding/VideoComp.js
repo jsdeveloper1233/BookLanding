ae3850ada2cdb4201ebbd1ceae8d47a8fb37d5be
Sergio Lo Porto <51946947+sergioloporto@users.noreply.github.com>
2021-05-21 22:40:42 +0200

    favicons done

--- components/BookLanding/VideoComp.js
+++ components/BookLanding/VideoComp.js
@@ -45,42 +45,7 @@ class VideoComp extends React.Component {
             </div>
           </div>
 
-          <div className="bg-gray shape-1"></div>
-          <div className="shape1">
-            <img src="/images/shape1.png" alt="img" />
-          </div>
-          <div className="shape2">
-            <img src="/images/shape2.png" alt="img" />
-          </div>
-          <div className="shape3">
-            <img src="/images/shape3.png" alt="img" />
-          </div>
-          <div className="shape6">
-            <img src="/images/shape6.png" alt="img" />
-          </div>
-          <div className="shape8 rotateme">
-            <img src="/images/shape8.svg" alt="shape" />
-          </div>
-          <div className="shape9">
-            <img src="/images/shape9.svg" alt="shape" />
-          </div>
-
-          <div className="bubble-animate">
-            <div className="circle small square1"></div>
-            <div className="circle small square2"></div>
-            <div className="circle small square3"></div>
-            <div className="circle small square4"></div>
-            <div className="circle small square5"></div>
-            <div className="circle medium square1"></div>
-            <div className="circle medium square2"></div>
-            <div className="circle medium square3"></div>
-            <div className="circle medium square4"></div>
-            <div className="circle medium square5"></div>
-            <div className="circle large square1"></div>
-            <div className="circle large square2"></div>
-            <div className="circle large square3"></div>
-            <div className="circle large square4"></div>
-          </div>
+        
         </div>
 
         <ModalVideo
