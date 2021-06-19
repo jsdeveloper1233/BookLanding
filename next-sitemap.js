39c73dade07f7c83b7d01cec11e84f07400a52fa
Sergio Lo Porto <sergioloporto@gmail.com>
2021-06-19 11:39:40 +0200

    added sitemap and exclusion list

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,4 +1,5 @@
 module.exports = {
     siteUrl: process.env.SITE_URL || process.env.development.SITE_URL,
     generateRobotsTxt: true, // (optional)
+    exclude: ['/checkout', '/czy-grupa-jest-sekta', '/thankyou'],
   }
