e0b879495da30b73d387f41365414d21e429e1f1
jsdeveloper1233 <ali1995ez@gmail.com>
2021-08-08 12:43:22 +0430

    fix page1 speed

--- pages/_document.js
+++ pages/_document.js
@@ -6,10 +6,6 @@ class MyDocument extends Document {
             <Html lang="pl">
                 <Head prefix="og: http://ogp.me/ns# book: http://ogp.me/ns/book#">
                     <link rel="icon" type="image/png" href="/images/favicon.png"></link>
-                    <link
-                        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-                        rel="stylesheet"
-                    />
                 </Head>
                 <body>
                     <Main />
